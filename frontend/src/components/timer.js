20c738e59aea49c269e4b81195c5c8cf280a3c3f
Maria Le <hp.mars.le@gmail.com>
2020-05-01 04:03:01 -0500

    edit render timer card

--- frontend/src/components/timer.js
+++ frontend/src/components/timer.js
@@ -2,7 +2,7 @@ class Timer {
     constructor(timer) {
         this.id = timer.id
         this.name = timer.attributes.name;
-        this.date = time.attributes.date;
+        this.date = timer.attributes.date;
         this.elapsed_time = timer.attributes.elapsed_time;
         this.category = timer.attributes.category;
     }
@@ -10,10 +10,13 @@ class Timer {
     renderTimer(){
         let timerCard = document.createElement('div')
         timerCard.classList.add('timer-card')
-        timerCard.innerHTML = `<p>${this.name}</p><p>${this.elapsed_time}</p><p>${this.category}</p><p>${this.category_color}</p>`
+        timerCard.innerHTML = `
+            <p>${this.name}</p><p class='timer-display'>${this.elapsed_time}</p><p>${this.category.name}</p>
+        `
+        timerCard.style.backgroundColor = `${this.category.key_color}`
         let controls = document.createElement('span')
         controls.classList.add('controls')
-        controls.innerHTML = "<span class='stopper'>start</span> <span class='reset'>reset</span>"
+        controls.innerHTML = "<button class='stopper'>start</button> <button class='reset'>reset</button>"
         timerCard.appendChild(controls)
         return timerCard
     }
@@ -25,39 +28,5 @@ class Timer {
 
     save()
 
-
-    timer.attributes
-    
-        // timersContainer.innerHTML = `${this.timers}`
-        this.timers.forEach(timer => {
-            const div = document.createElement("div")
-            div.className = "timer-container"
-            
-            const ul = document.createElement("ul")
-            
-            const li = document.createElement("li")
-            
-            li.innerHTML = Object.keys(timer).map(k => {
-                return k.includes('_') ? `<li>${k.split('_').join(' ')}: ${timer[k]}</li>` : `<li>${k}: ${timer[k]}</li>`
-                // if (k.includes('_')){
-                //     return `<li>${k.split('_').join(' ')}</li>`
-                // } else {
-                //     return `<li>${k}</li>`
-                // }
-            }).join('')
-
-            ul.appendChild(li)
-            div.appendChild(ul)
-            // for (k in timer) {
-                // console.log(k, timer[k])
-                // let li = document.createElement("li")
-                // li.innerHTML = k
-                // div.appendChild(li)
-            // }
-            // Object.keys(timer).map( k => k: )
-
-
-            // li.innerHTML = + Object.values(timer).join(' ')
-            timersContainer.appendChild(div)
-            */
+    */
 }
