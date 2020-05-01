58405cead15b5b8d9b366baa33e806879a9331bf
Maria Le <hp.mars.le@gmail.com>
2020-05-01 03:44:00 -0500

    updated controller actions

--- backend/app/controllers/api/v1/timers_controller.rb
+++ backend/app/controllers/api/v1/timers_controller.rb
@@ -7,21 +7,22 @@ class Api::V1::TimersController < ApplicationController
 
     def show
         timer = Timer.find_by(id: params[:id])
-        options = {
-            include: [:category]
-        }
-        render json: TimerSerializer.new(timer, options)
+ 
+        render json: TimerSerializer.new(timer)
 
     end
 
     def create 
-        @timer = Timer.create(timer_params)
-        render json: @timer 
+        timer = Timer.create(timer_params)
+        category = Category.find_by(name: params[:category]) #might need to do name instead? how do I post this from select options on form into the fetch body?
+        timer.category = category
+        timer.save
+        render json: TimerSerializer.new(timer)
     end
 
     private 
 
     def timer_params
-        params.require(:timer).permit(:name, :date, :elapsed_time, :category_id)
+        params.require(:timer).permit(:name, :date, :elapsed_time, :category)
     end
 end
