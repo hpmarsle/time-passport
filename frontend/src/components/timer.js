15171deea6029e6cbe966c25a9abdd5e83c6cfe5
Maria Le <hp.mars.le@gmail.com>
2020-05-04 15:57:37 -0500

    started stopper functionality (not working yet), load event listeners after all timers are loaded on dom, fetch delete request works

--- frontend/src/components/timer.js
+++ frontend/src/components/timer.js
@@ -10,23 +10,44 @@ class Timer {
     renderTimer(){
         let timerCard = document.createElement('div')
         timerCard.classList.add('timer-card')
+        timerCard.setAttribute('id', `${this.id}`)
         timerCard.innerHTML = `
             <p>${this.name}</p><p class='timer-display'>${this.elapsed_time}</p><p>${this.category.name}</p>
         `
         timerCard.style.backgroundColor = `${this.category.key_color}`
         let controls = document.createElement('span')
         controls.classList.add('controls')
-        controls.innerHTML = "<button class='stopper'>start</button> <button class='reset'>reset</button>"
+        controls.innerHTML = "<button class='stopper'>start</button> <button class='reset'>reset</button> <button class='delete'>delete</button>"
         timerCard.appendChild(controls)
         return timerCard
     }
-    /*
-   
-    start()
+    //sidebar this
+    static start(timeArr) {
+        let h = parseInt(timeArr[0])
+        let m = parseInt(timeArr[1])
+        let s = parseInt(timeArr[2])
+        
+        s++;
+        if (s == 60){
+            s = 0
+            m++
 
+            if (m == 60) {
+                m = 0
+                h++
+            }
+        }
+        h = ('00' + h.toString()).substr(-2);
+        m = ('00' + m.toString()).substr(-2);
+        s = ('00' + s.toString()).substr(-2);
+        let newTime = [h, m, s]
+        return newTime
+    }
+    
+/*
     pause()
 
     save()
 
     */
-}
+} 
--- frontend/src/components/timers.js
+++ frontend/src/components/timers.js
@@ -4,30 +4,14 @@ class Timers {
         this.adapter = new TimersAdapter();
         this.initBindings();
         this.fetchAndLoadTimers();
-        this.loadEventListeners();
+        // this.loadEventListeners();
     }
 
     initBindings() {
-
         this.timersContainer = document.getElementById("timers-container") 
-        this.newTimerForm = document.getElementById('new-timer-form')
-        this.timerDisplays = document.querySelectorAll('.timer-display')
-        this.stopperButtons = document.querySelectorAll('.stopper')
-        
+        this.newTimerForm = document.getElementById("new-timer-form")    
     }
 
-    loadEventListeners() {
-        this.newTimerForm.addEventListener('submit', (e) => {
-            e.preventDefault()
-            this.createTimer(e.target)
-        })
-        /*
-        this.timerDisplays.addEventListener('click', e => console.log('clicked!')) 
-        when start button is clicked change display of timer to count up set interval 1000 */
-        ``
-    }
-        
-    
     createTimer(form) {
         let configObj = {
             method: "POST",
@@ -54,11 +38,7 @@ class Timers {
             let newTimer = new Timer(timer.data)
             this.timersContainer.appendChild(newTimer.renderTimer())
         })
-        .catch(err => {throw err})
-    
-
-        
-        
+        .catch(err => {throw err})     
     }
     
     fetchAndLoadTimers() {
@@ -70,13 +50,83 @@ class Timers {
         })
         .then(() => {
             this.makeTimerCards()
+        }).then(() => {
+            this.loadEventListeners()
+        })
+    }
+
+    deleteTimer(id) {
+        fetch(`${this.adapter.baseUrl}/${id}`, {
+            method: 'DELETE'
         })
     }
 
     makeTimerCards(){
-        
         this.timers.forEach(timer => this.timersContainer.appendChild(timer.renderTimer()))
-
-        console.log('div added to container')
     }
+
+    loadEventListeners() {
+
+        this.newTimerForm.addEventListener('submit', (e) => {
+            e.preventDefault()
+            this.createTimer(e.target)
+        })
+        this.timerDisplays = document.querySelectorAll(".timer-display")
+        this.stopperButtons = document.getElementsByClassName('stopper');
+        this.resetButtons = document.querySelectorAll(".reset")
+        this.deleteButtons = document.getElementsByClassName('delete');
+
+        for (const d of this.deleteButtons) {
+            d.addEventListener('click', e => {
+                const timerCard = e.target.parentElement.parentElement
+                this.deleteTimer(timerCard.id)
+                timerCard.remove();
+            })
+
+        }
+        for (const stopper of this.stopperButtons) {
+            stopper.addEventListener('click', (e) => {
+                if (e.target.innerText === 'start') {
+                    e.target.innerText = 'stop'
+                    const timerDisplay = e.target.parentElement.parentElement.querySelector('.timer-display')
+                    let time = timerDisplay.innerText.split(':')
+                    //sidebar this
+                    setInterval(() => {
+                        let h = time[0]
+                        let m = time[1]
+                        let s = time[2]
+
+                        s++;
+                        if (s == 60){
+                            s = 0
+                            m++
+
+                            if (m == 60) {
+                                m = 0
+                                h++
+                            }
+                        }
+                        h = ('00' + h.toString()).substr(-2);
+                        m = ('00' + m.toString()).substr(-2);
+                        s = ('00' + s.toString()).substr(-2);
+                        let newTime = [h, m, s].join(':')
+                        timerDisplay.innerText = newTime
+                    }, 1000)
+
+                    //   start interval timer.
+                } else {
+                    e.target.innerText = 'start'
+                    //freeze timer
+                }
+            })
+        }
+    
+        
+    }
+
+    toggleStopper() {
+        console.log('hit stopper!')
+        //after above works turn innerText to stop if start and vv (vice versa)
+    }
+    
 }
