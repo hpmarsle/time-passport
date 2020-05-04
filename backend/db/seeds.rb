4cf0b7957e3f28d09a958e95a0ef045e0592991a
Maria Le <hp.mars.le@gmail.com>
2020-05-04 15:54:43 -0500

    edit seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,13 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup)
 categories = Category.create([
-    # {name: "Work", key_color: "blue"}, made in console
-    {name: "Leisure", key_color: "yellow"},
-    {name: "Health", key_color: "pink"}
+    {name: "Work", key_color: "LightSteelBlue"},
+    {name: "Leisure", key_color: "Peru"},
+    {name: "Health", key_color: "#FFB6C1"}
 ])
 
 timers = Timer.create([
-    # {name: "Coding", elapsed_time: "00:00:00", date: "4/16/2020", category_id: 1}, made in console
+    {name: "Coding", elapsed_time: "00:00:00", date: "4/16/2020", category_id: 1},
     {name: "Workout", elapsed_time: "00:00:00", date: "4/16/2020", category_id: 3},
     {name: "Youtube", elapsed_time: "00:00:00", date: "4/16/2020", category_id: 2},
     {name: "Blog", elapsed_time: "00:00:00", date: "4/16/2020", category_id: 1},
